c7a086b4304cb2f6f86f7d32f12d11b9a9158d8a
mohamed <mohamed_gsm@msn.com>
2021-10-04 19:35:13 +0200

    create provides of repository

--- app/src/main/java/com/example/artbookhilttesting/di/AppModule.kt
+++ app/src/main/java/com/example/artbookhilttesting/di/AppModule.kt
@@ -2,10 +2,19 @@ package com.example.artbookhilttesting.di
 
 import android.content.Context
 import androidx.room.Room
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.artbookhilttesting.R
 import com.example.artbookhilttesting.api.EndPoint
 import com.example.artbookhilttesting.constants.Const
 import com.example.artbookhilttesting.room.ArtDao
 import com.example.artbookhilttesting.room.ArtDatabase
+import com.example.artbookhilttesting.ui.fragment.adddetailsfragment.AddArtDetailsRepository
+import com.example.artbookhilttesting.ui.fragment.adddetailsfragment.AddArtDetailsRepositoryInterface
+import com.example.artbookhilttesting.ui.fragment.artsfragment.ArtRepository
+import com.example.artbookhilttesting.ui.fragment.artsfragment.ArtRepositoryInterface
+import com.example.artbookhilttesting.ui.fragment.imageapifragment.ImageApiRepository
+import com.example.artbookhilttesting.ui.fragment.imageapifragment.ImageApiRepositoryInterface
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -41,5 +50,28 @@ object AppModule {
            .build().create(EndPoint::class.java)
     }
 
+    /* provide ArtRepository */
+    @Singleton
+    @Provides
+    fun injectNormalArtRepo( artDao: ArtDao , api : EndPoint) = ArtRepository( artDao , api ) as ArtRepositoryInterface
+
+    /* provide AddArtDetailsRepository */
+    @Singleton
+    @Provides
+    fun injectNormalAddArtDetailsRepo( artDao: ArtDao , api : EndPoint) = AddArtDetailsRepository( artDao , api ) as AddArtDetailsRepositoryInterface
+
+    /* provide ImageApiRepository */
+    @Singleton
+    @Provides
+    fun injectNormalImageApiRepo( artDao: ArtDao , api : EndPoint) = ImageApiRepository( artDao , api ) as ImageApiRepositoryInterface
+
+    /* provide adapter */
+    @Singleton
+    @Provides
+    fun injectGlide( @ApplicationContext context: Context ) = Glide.with(context)
+        .setDefaultRequestOptions(
+            RequestOptions().placeholder(R.drawable.default_image)
+                .error(R.drawable.default_image)
+        )
 
 }
